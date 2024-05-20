b01865b270da09f3c8b10c67a575134885001b4d
suryahidayat <surya.hidayat@inmotion.co.id>
2024-05-20 16:26:09 +0700

    clean template and add router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import Home from '../src/components/Home.vue'
+import NotFound from '../src/components/NotFound.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: Home },
+        { path: '/:pathMatch(.*)*', component: NotFound }
+    ]
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
